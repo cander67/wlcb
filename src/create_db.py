e18a9201cb09c060c8062dbd341cdf108ece2890
Cyrus Anderson <anderson.cyrus@gmail.com>
2024-03-06 13:04:21 -0800

    script: insert data

--- src/create_db.py
+++ src/create_db.py
@@ -1,11 +1,13 @@
 import utils
-import time
 
 # Define location of license and sales files, map names to paths
-data_files = utils.get_valid_data_files("./data")
+files = utils.get_valid_data_files("./data")
 
 # Create SQL database and changelog for WLCB license and sales data
 db = utils.WLCB_DB('wlcb_db')
 
 # Create datatables
 db.create_datatables()
+
+# Populate datatables with license and sales data
+db.insert_data(files)
