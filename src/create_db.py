50479b920efec3e29231c4b14f0232b78c39fd33
Cyrus Anderson <anderson.cyrus@gmail.com>
2024-03-06 10:38:42 -0800

    script: check for datafiles and create database

--- src/create_db.py
+++ src/create_db.py
@@ -1,2 +1,8 @@
 import utils
 import time
+
+# Define location of license and sales files, map names to paths
+data_files = utils.get_valid_data_files("./data")
+
+# Create SQL database for WLCB license and sales data
+db = utils.WLCB_DB('wlcb_db')
